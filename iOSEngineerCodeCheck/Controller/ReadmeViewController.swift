9f9fbbce19363470eeb6dd5015ab368156afc3ce
Eriko-creator <koenuma.eri@gmail.com>
2021-04-10 00:41:53 +0900

    継承しないのでfinalをつけた

--- iOSEngineerCodeCheck/Controller/ReadmeViewController.swift
+++ iOSEngineerCodeCheck/Controller/ReadmeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import MarkdownView
 
-class ReadmeViewController: UIViewController {
+final class ReadmeViewController: UIViewController {
     
     private let md = MarkdownView()
     
